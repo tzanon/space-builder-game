f8d8ec9aea3e87afe94bca8ede713a6200e47fc8
Tyler Zanon <tdzanon@gmail.com>
2017-06-23 00:49:15 -0400

    first commit: made arena box and moving player

---
+++ Space Builder/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float moveFactor = 15f;
+    private float rotateFactor = 30f;
+
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        // moving/strafing
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += (moveFactor * Time.deltaTime) * transform.forward.normalized;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= (moveFactor * Time.deltaTime) * transform.forward.normalized;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += (moveFactor * Time.deltaTime) * transform.right.normalized;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= (moveFactor * Time.deltaTime) * transform.right.normalized;
+        }
+
+        // rotating
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(rotateFactor * Time.deltaTime * Vector3.left);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(rotateFactor * Time.deltaTime * Vector3.right);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(rotateFactor * Time.deltaTime * Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(rotateFactor * Time.deltaTime * Vector3.down);
+        }
+
+    }
+
+}
